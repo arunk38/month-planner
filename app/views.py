2c621328416387334d0ca1dbaa44772a2404bda7
Arun Kumar <karun.2210@gmail.com>
2017-09-30 13:35:34 +0530

    Templates in Flask webapp
      * Basic html templates
    	* if and for loops in templates
    	* Create and use common template(base.html)

--- app/views.py
+++ app/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Arun Kumar'} #fake user
+    posts = [ #fake array of posts
+                {
+                    'author': {'nickname': 'Arun'},
+                    'body'  : 'Beautiful day today!'
+                },
+                {
+                    'author': {'nickname': 'Anna'},
+                    'body'  : 'The Avengers movie was so cool!'
+                }
+            ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            posts=posts)
